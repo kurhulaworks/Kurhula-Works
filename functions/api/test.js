7277698630d83cea10eac8566c47f80330b5f5fa
kurhulaworks <kurhulaworks@gmail.com>
2026-09-09 20:56:30 +0200

    Update test.js

--- functions/api/test.js
+++ functions/api/test.js
@@ -1,1 +1,14 @@
 
+export function onRequest() {
+  return new Response(
+    JSON.stringify({
+      success: true,
+      message: "Kurhula Works Pages Function works."
+    }),
+    {
+      headers: {
+        "Content-Type": "application/json"
+      }
+    }
+  );
+}
