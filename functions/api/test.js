3e3d061883d2564647983571765ac37e66257265
kurhulaworks <kurhulaworks@gmail.com>
2026-09-10 17:26:58 +0200

    Update test.js

--- functions/api/test.js
+++ functions/api/test.js
@@ -1,13 +1,33 @@
-export function onRequest() {
-  return new Response(
-    JSON.stringify({
-      success: true,
-      message: "Kurhula Works Pages Function works."
-    }),
-    {
+export async function onRequest(context) {
+  try {
+    const response = await fetch(
+      "https://kurhula-works-api.kurhulaworks.workers.dev/github-test",
+      {
+        method: "GET"
+      }
+    );
+
+    const data = await response.text();
+
+    return new Response(data, {
+      status: response.status,
       headers: {
         "Content-Type": "application/json"
       }
-    }
-  );
+    });
+
+  } catch (error) {
+    return new Response(
+      JSON.stringify({
+        success: false,
+        error: "Unable to connect to Worker."
+      }),
+      {
+        status: 500,
+        headers: {
+          "Content-Type": "application/json"
+        }
+      }
+    );
+  }
 }
