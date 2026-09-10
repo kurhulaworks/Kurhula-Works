823cd56b95e5af239497878545b65438d5f76b01
kurhulaworks <kurhulaworks@gmail.com>
2026-09-10 18:12:01 +0200

    Update test.js

--- functions/api/test.js
+++ functions/api/test.js
@@ -1,15 +1,27 @@
 export async function onRequest(context) {
-  return new Response(
-    JSON.stringify({
-      hasEnv: !!context.env,
-      hasService: !!context.env?.SERVICE,
-      envKeys: context.env ? Object.keys(context.env) : []
-    }),
-    {
-      headers: {
-        "Content-Type": "application/json"
+  try {
+    const request = new Request(
+      "https://kurhula-works-api/github-test",
+      {
+        method: "GET"
       }
-    }
-  );
-}
+    );
 
+    return await context.env.SERVICE.fetch(request);
+
+  } catch (error) {
+    return new Response(
+      JSON.stringify({
+        success: false,
+        error: "Unable to connect to Worker.",
+        details: error.message
+      }),
+      {
+        status: 500,
+        headers: {
+          "Content-Type": "application/json"
+        }
+      }
+    );
+  }
+}
